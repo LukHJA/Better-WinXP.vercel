04b0c9e09ab1e7413ce198e686ed58ac7ac6e216
LukHJA <86873315+LukHJA@users.noreply.github.com>
2022-07-15 13:52:43 +0300

    Update RemadeStartMenu.js

--- JScodes/RemadeStartMenu.js
+++ JScodes/RemadeStartMenu.js
@@ -2,7 +2,7 @@
   var StartMenu = document.querySelector("#root > div > footer > div.footer__items.left > img");
   StartMenu.setAttribute("src", "");
   StartMenu.setAttribute("alt", "");
-  StartMenu.innerHTML = ['<div id="StartMenu"><img id="StartMenuImage"></img><p id="StartMenuText">Start</p></div>'];
+  StartMenu.innerHTML = ['<div id="StartMenu"></div>'];
   var StartMenuNew = document.getElementById("StartMenu");
   StartMenuNew.setAttribute("title", "Click here to begin");
 })();
